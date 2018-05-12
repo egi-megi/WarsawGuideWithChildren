05aeb98f1f71a0a714edcb6da5990a879a0f4d77
egi-megi <egi.megi@gmail.com>
2018-05-12 21:57:08 +0200

    working back button

--- app/src/main/java/com/example/android/warsawguidewithchildren/MainFragment.java
+++ app/src/main/java/com/example/android/warsawguidewithchildren/MainFragment.java
@@ -3,6 +3,7 @@ package com.example.android.warsawguidewithchildren;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
@@ -15,36 +16,43 @@ public class MainFragment extends Fragment {
      ViewPager viewPager;
     CategoryAdapter adapter;
     TabLayout tabLayout;
+    View rootView=null;
+
     @Override
     public void onResume() {
         super.onResume();
-        viewPager = (ViewPager) getView().findViewById(R.id.viewpager);
-        adapter = new CategoryAdapter(getActivity(), getActivity().getSupportFragmentManager());
-        viewPager.setAdapter(adapter);
-        // Give the TabLayout the ViewPager
-        tabLayout = (TabLayout) getView().findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
-        int i=tabLayout.getSelectedTabPosition();
-        viewPager.setCurrentItem(i);
 
+
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        // Find the view pager that will allow the user to swipe between fragments
-        viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
+        if (rootView==null) {
+            rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            // Find the view pager that will allow the user to swipe between fragments
+            viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
 
-        // Create an adapter that knows which fragment should be shown on each page
-        adapter = new CategoryAdapter(getActivity(), getActivity().getSupportFragmentManager());
+            // Create an adapter that knows which fragment should be shown on each page
+            adapter = new CategoryAdapter(getActivity(), getActivity().getSupportFragmentManager());
 
-        // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
+            // Set the adapter onto the view pager
+            viewPager.setAdapter(adapter);
 
-        // Give the TabLayout the ViewPager
-        tabLayout = (TabLayout) rootView.findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+            // Give the TabLayout the ViewPager
+            tabLayout = (TabLayout) rootView.findViewById(R.id.sliding_tabs);
+            tabLayout.setupWithViewPager(viewPager);
+        } else {
+            if (rootView.getParent()!=null) {
+                ((ViewGroup) rootView.getParent()).removeView(rootView);
+            }
+        }
         return rootView;
     }
 
